2b549c53c54f1c5a848d8bece5c5227ac81eefe6
mensi93 <mensi.mou@gmail.com>
2025-09-29 13:38:27 +0200

    Update ProductTrialDbContext.cs

--- back/productMasterTrial/productMasterTrial/Alten.ProductMasterTrial.Infrastructure/Persistance/ProductTrialDbContext/ProductTrialDbContext.cs
+++ back/productMasterTrial/productMasterTrial/Alten.ProductMasterTrial.Infrastructure/Persistance/ProductTrialDbContext/ProductTrialDbContext.cs
@@ -1,12 +1,13 @@
-﻿using Alten.ProductMaster.Domain.Entities;
+using Alten.ProductMaster.Domain.Entities;
 using Alten.ProductMaster.Infrastructure.Persistance.Configuration;
 using Alten.ProductMasterTrial.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Alten.ProductMasterTrial.Infrastructure.Persistance
 {
-    public  class ProductTrialDbContext : DbContext
+    public class ProductTrialDbContext : DbContext
     {
+
 
         public DbSet<Product> Products => Set<Product>();
         public DbSet<Member> Members => Set<Member>();
@@ -14,19 +15,10 @@ namespace Alten.ProductMasterTrial.Infrastructure.Persistance
         public DbSet<CartItem> CartItems => Set<CartItem>();
         public DbSet<WishList> WishLists => Set<WishList>();
         public DbSet<WishlistItem> WishlistItems => Set<WishlistItem>();
-
         public ProductTrialDbContext(DbContextOptions<ProductTrialDbContext> options)
             : base(options) { }
 
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                //todo :supprimer la cnx string
-                optionsBuilder.UseSqlServer("Server=(LocalDb)\\MSSQLLocalDB;Database=ProductTrialDb;Integrated Security=True;Trusted_Connection=True;");
-            }
-        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
